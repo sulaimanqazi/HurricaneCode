d502d9f23ec19a6ded25ada623ebb6982cc0a633
Molotov <irfaankhan2008@gmail.com>
2025-10-15 14:07:10 -0400

    Update TeleopField.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopField.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopField.java
@@ -62,8 +62,8 @@ public class TeleopField extends OpMode {
 
         // We set the left motors in reverse which is needed for drive trains where the left
         // motors are opposite to the right ones.
-        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
-        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        backRightDrive.setDirection(DcMotor.Direction.REVERSE);
 
         // This uses RUN_USING_ENCODER to be more accurate.   If you don't have the encoder
         // wires, you should remove these
