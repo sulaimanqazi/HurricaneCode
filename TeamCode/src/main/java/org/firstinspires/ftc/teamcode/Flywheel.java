9f999f515ac41733ba4e96ce3523cf85d8bf65fa
Kevin H <104533338+Toastyybread1@users.noreply.github.com>
2025-11-04 22:54:09 -0500

    Flywheel subsystem
    needs testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Flywheel.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.guide.java.subsystems;
+
+import dev.nextftc.control.ControlSystem;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.hardware.controllable.RunToVelocity;
+import dev.nextftc.hardware.impl.MotorEx;
+
+public class Flywheel implements Subsystem {
+    public static final Flywheel INSTANCE = new Flywheel();
+    private Flywheel() { }
+
+    private final MotorEx motor = new MotorEx("flywheel_motor");
+
+    private final ControlSystem controller = ControlSystem.builder()
+            .velPid(0.005, 0, 0)
+            .basicFF(0.01, 0.02, 0.03)
+            .build();
+
+    public final Command off = new RunToVelocity(controller, 0.0).requires(this).named("FlywheelOff");
+
+    // tweak RPM in this command we want it to be at 2000
+    public final Command on = new RunToVelocity(controller, 2000.0).requires(this).named("FlywheelOn");
+
+    @Override
+    // runs every loop
+    public void periodic() {
+
+        // see if the goal is measured in RPM or something else
+        System.out.println("Flywheel velocity: " + motor.getState());
+        motor.setPower(controller.calculate(motor.getState()));
+    }
+}
