97a7f9b1743864bce9aa96b52b6721de6980dc25
Sulaiman Qazi <sulaimanqazi14@gmail.com>
2025-11-06 16:13:34 -0500

    teleop fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop.java
@@ -1,11 +1,51 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
-public class teleop {
 
-    
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.ftc.Gamepads;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.ftc.components.BulkReadComponent;
+import dev.nextftc.hardware.driving.MecanumDriverControlled;
+import dev.nextftc.hardware.impl.MotorEx;
+
+import org.firstinspires.ftc.teamcode.guide.java.subsystems.Flywheel;
+import org.firstinspires.ftc.teamcode.subsystems.*;
+
+public class teleop extends NextFTCOpMode{
+    public teleop() {
+        addComponents(
+                new SubsystemComponent(Transfer.INSTANCE, TransferServo.INSTANCE, org.firstinspires.ftc.teamcode.guide.java.subsystems.Flywheel.INSTANCE),
+                BulkReadComponent.INSTANCE,
+                BindingsComponent.INSTANCE
+        );
+    }
+
+
+    @Override
+    public void onStartButtonPressed() {
+        Gamepads.gamepad1().a()
+                .whenBecomesTrue(org.firstinspires.ftc.teamcode.guide.java.subsystems.Flywheel.INSTANCE.on)
+                .whenBecomesFalse(org.firstinspires.ftc.teamcode.guide.java.subsystems.Flywheel.INSTANCE.off);
+
+        // Transfer controls - B button
+        Gamepads.gamepad1().b()
+                .whenBecomesTrue(Transfer.INSTANCE.on)
+                .whenBecomesFalse(Transfer.INSTANCE.off);
+
+        // Transfer Servo controls - X button
+        Gamepads.gamepad1().x()
+                .whenBecomesTrue(TransferServo.INSTANCE.open)
+                .whenBecomesFalse(TransferServo.INSTANCE.close);
+
+
+    }
 
 
 
 
 }
+
